2a762ac2fecb54d02a4dcac3029666ed66cf0843
hiteshsubnani0128 <hiteshsubnani734@gmail.com>
2019-05-13 10:32:06 +0530

    Vowel or Consonent

---
+++ vowelOrConst.py
@@ -0,0 +1,12 @@
+# Program to Find wheather a character is Vowel or consonent
+def vOrC(chr):
+    if chr.lower() in 'aeiou':
+        return f"{chr} is a Vowel"
+    else:
+        return f"{chr} is a Consonent"
+
+if __name__=='__main__':
+    print(vOrC(input("Enter a Character ")))
+
+# Enter a Character E
+# E is a Vowel
