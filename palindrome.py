bc00482ed4150d3624ee77b7d6d62064cef82be8
hiteshsubnani0128 <hiteshsubnani734@gmail.com>
2019-05-13 10:56:10 +0530

    ADDED Palindrome

--- palindrome.py
+++ palindrome.py
@@ -7,10 +7,29 @@ def palindrome(n):
     else:
         return f"{n} is Not a Palindrome"
     
-
+def palindrome(n):
+    if str(n) == str(n)[::-1]:
+        return f"{n} is a Palindrome"
+    else:
+        return f"{n} is Not a Palindrome"
+    
 if __name__ == '__main__':
     num = int(input("Enter a Number "))
     print(palindrome(num))
 
+
 # Enter a Number 1001001
 # 1001001 is a Palindrome
+
+# /*---------------==========================================-------------------*/
+# Other Possible Solutions
+
+# def palindrome(num):
+#     temp = num
+#     rev = 0
+#     while num!=0:
+#         s = num%10
+#         rev = rev*10 + s
+#         num = num//10
+#     return temp==rev
+
